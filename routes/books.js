75e411029ff659af23ae6ab12f57a5bb4d31a3cb
jsultana <148451711+jsultana@users.noreply.github.com>
2025-11-16 22:53:44 +0000

    Complete Lab 6 Part D and E (bargain books + LIKE search)

--- routes/books.js
+++ routes/books.js
@@ -1,15 +1,49 @@
 // Create a new router
 const express = require('express');
 const router = express.Router();
+const db = global.db;
 
 // Show the search form
 router.get('/search', function (req, res, next) {
     res.render('search.ejs');
 });
 
-// Handle search result (just echoing the keyword for now)
+// Show the form to add a new book
+router.get('/addbook', function (req, res, next) {
+    res.render('addbook.ejs');
+});
+
+// Handle the form submission and insert a new book
+router.post('/bookadded', function (req, res, next) {
+    let sqlquery = "INSERT INTO books (name, price) VALUES (?, ?)";
+    let newrecord = [req.body.name, req.body.price];
+
+    db.query(sqlquery, newrecord, (err, result) => {
+        if (err) {
+            next(err);
+        } else {
+            res.send(
+                'This book was added to the database: name = ' +
+                req.body.name +
+                ', price = ' +
+                req.body.price
+            );
+        }
+    });
+});
+
+// Handle search result - search for books by name (LIKE search)
 router.get('/search-result', function (req, res, next) {
-    res.send('You searched for: ' + req.query.keyword);
+    let keyword = req.query.keyword;
+    let sqlquery = "SELECT * FROM books WHERE name LIKE ?";
+    let term = "%" + keyword + "%";
+
+    db.query(sqlquery, [term], (err, result) => {
+        if (err) {
+            return next(err);
+        }
+        res.render("list.ejs", { availableBooks: result });
+    });
 });
 
 // List all books from the database
@@ -20,7 +54,19 @@ router.get('/list', function (req, res, next) {
         if (err) {
             return next(err); // pass error to Express error handler
         }
-        res.send(result); // send back data as JSON
+        res.render("list.ejs", { availableBooks: result });
+    });
+});
+
+// List all books cheaper than £20 (bargain books)
+router.get('/bargainbooks', function (req, res, next) {
+    const sqlquery = 'SELECT * FROM books WHERE price < 20';
+
+    db.query(sqlquery, (err, result) => {
+        if (err) {
+            return next(err);
+        }
+        res.render("list.ejs", { availableBooks: result });
     });
 });
 
